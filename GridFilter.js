2a75f22ba27f601794de9d9a5d56ddf7de21c7b0
Roberto <titorobe@yahoo.com>
2016-07-11 14:53:08 -0400

    Supporting columns sort.

--- GridFilter.js
+++ GridFilter.js
@@ -27,29 +27,29 @@
  *   
  *   
  * 
-  Example:
+ Example:
  
-  var grid = new Ext.grid.GridPanel({
-  columns: [
-     {
-        text: "User ID",
-        dataIndex: 'userId', 
-        filter: true
-    },
-    {
-        text: "First Name",
-        dataIndex: 'firstName', 
-        filter: true
-    },
-    {
-        text: "First Name",
-        dataIndex: 'firstName', 
-        filter: lastName
-    }
-  ],
-  plugins:[{ptype:"gridFilter"}]
-  ...
-});
+ var grid = new Ext.grid.GridPanel({
+ columns: [
+ {
+ text: "User ID",
+ dataIndex: 'userId', 
+ filter: true
+ },
+ {
+ text: "First Name",
+ dataIndex: 'firstName', 
+ filter: true
+ },
+ {
+ text: "First Name",
+ dataIndex: 'firstName', 
+ filter: lastName
+ }
+ ],
+ plugins:[{ptype:"gridFilter"}]
+ ...
+ });
  */
 
 Ext.define('Ext.ux.grid.GridFilter', {
@@ -57,32 +57,42 @@ Ext.define('Ext.ux.grid.GridFilter', {
     alias: 'plugin.gridFilter',
     init: function (grid) {
         var me = this;
-        grid.relayEvents(grid.getStore(), ['load','beforeload','sortchange']);
+        grid.relayEvents(grid.getStore(), ['viewready','load', 'beforeload', 'sortchange']);
         grid.addListener('beforeload', me.onBeforeLoad);
+        grid.addListener('viewready', me.storeload);
         grid.addListener('load', me.storeload);
         grid.addListener('sortchange', me.storeload);
-    }, 
-    storeload: function () {
+    },
+    storeload: function () { 
         var me = this;
-
         var body = me.body.dom;
-
-        var table = body.childNodes[0].getElementsByClassName('x-grid-table')[0];
+ 
+        var table = body.getElementsByClassName('x-grid-table')[0];
+        
+        if(!table){  
+             return;
+        }
+        
         var tbody = table.getElementsByTagName('tbody')[0];
 
         var tr1 = tbody.childNodes[0];
         
-        if(tr1){
+        if(tr1.classList.contains('ux-grid-filter')){
+            return;
+        } 
+
+        if (tr1) {
             me['tr1'] = tr1;
-        }else{
-           if(me['tr1']){
-               tr1 = me['tr1'];
-           }else{
-               return;
-           }
+        } else {
+            if (me['tr1']) {
+                tr1 = me['tr1'];
+            } else {
+                return;
+            }
         }
 
         var newTr = document.createElement('tr');
+        newTr.setAttribute("class", "ux-grid-filter");
 
         for (var i = 0; i < tr1.childNodes.length; i++) {
 
@@ -91,39 +101,39 @@ Ext.define('Ext.ux.grid.GridFilter', {
             td.setAttribute("class", "x-grid-cell-special");
             td.setAttribute("style", "border: solid gray 1px;");
 
-            if (me.columns[i].initialConfig.filter) { 
+            if (me.columns[i].initialConfig.filter) {
                 td.setAttribute("style", "width:100%;padding:2px 5px;border: solid gray 1px;");
 
                 var input = document.createElement('input');
                 input.setAttribute("style", "width:100%");
                 input.setAttribute("name", me.columns[i].initialConfig.dataIndex);
 
-                if(me.filters && me.filters[me.columns[i].initialConfig.dataIndex]){
+                if (me.filters && me.filters[me.columns[i].initialConfig.dataIndex]) {
                     input.value = me.filters[me.columns[i].initialConfig.dataIndex];
                 }
 
                 input.onkeyup = function (event) {
                     if (event.keyCode === 13) {
                         var tr = event.target.parentNode.parentNode;
-                        
+
                         var searchParams = {};
-                        
+
                         for (var j = 0; j < tr.childNodes.length; j++) {
                             if (tr.childNodes[j].childNodes[0] && tr.childNodes[j].childNodes[0].value) {
-                              searchParams[tr.childNodes[j].childNodes[0].name] = tr.childNodes[j].childNodes[0].value;
-                            } 
-                        } 
+                                searchParams[tr.childNodes[j].childNodes[0].name] = tr.childNodes[j].childNodes[0].value;
+                            }
+                        }
                         me['filters'] = searchParams;
-                        
+
                         var clonedSearchParams = {//for avoid include start and page in the global var
-                            start:0,
-                            page:1
+                            start: 0,
+                            page: 1
                         };
-                        
+
                         for (var key in searchParams) {
                             clonedSearchParams[key] = searchParams[key];
                         }
-                        
+
                         me.store.load({params: clonedSearchParams});
                         me.store.currentPage = 1;
                     }
@@ -132,19 +142,18 @@ Ext.define('Ext.ux.grid.GridFilter', {
             }
             newTr.appendChild(td);
         }
-        
-        if(tbody.childNodes.length > 0){
-           tbody.insertBefore(newTr, tr1); 
-        }else{
-           tbody.appendChild(newTr); 
+
+        if (tbody.childNodes.length > 0) {
+            tbody.insertBefore(newTr, tr1);
+        } else {
+            tbody.appendChild(newTr);
         }
-        
+
     },
-    
-    onBeforeLoad: function( store, operation, eOpts ){
-        var me = this; 
-       if(me.filters){
-           store.getProxy().extraParams = me.filters;
-       } 
-    } 
+    onBeforeLoad: function (store, operation, eOpts) {
+        var me = this;
+        if (me.filters) {
+            store.getProxy().extraParams = me.filters;
+        }
+    }
 });
